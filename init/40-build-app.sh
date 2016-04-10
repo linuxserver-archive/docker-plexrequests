01110229e0adf9960f88a878be6a2bc7e7e2df43
zaggash <zaggash@users.noreply.github.com>
2016-04-11 01:22:46 +0200

    Create Build init file

---
+++ init/40-build-app.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+##Variables
+COPIED_APP_PATH="/tmp/git-app"
+BUNDLE_DIR="/tmp/bundle-dir"
+VERSION=$(curl -sX GET  "https://api.github.com/repos/lokenx/plexrequests-meteor/releases/latest" | awk '/tag_name/{print $4;exit}' FS='[""]')
+
+##Execution
+curl -o /tmp/source.tar.gz -L https://github.com/lokenx/plexrequests-meteor/archive/$VERSION.tar.gz && \
+mkdir -p $COPIED_APP_PATH && \
+tar xvf /tmp/source.tar.gz -C $COPIED_APP_PATH --strip-components=1 && \
+cd $COPIED_APP_PATH && \
+HOME=/tmp curl -sL https://install.meteor.com | sed s/--progress-bar/-sL/g | /bin/sh && \
+HOME=/tmp meteor build --directory $BUNDLE_DIR --server=http://localhost:3000 && \
+cd $BUNDLE_DIR/bundle/programs/server/ && \
+npm i && \
+mv $BUNDLE_DIR/bundle /app && \
+rm /usr/local/bin/meteor && \
+rm -rf /usr/share/doc /usr/share/doc-base && \
+npm cache clear > /dev/null 2>&1 && \
+rm -rf /tmp/* /tmp/.??* /root/.meteor
